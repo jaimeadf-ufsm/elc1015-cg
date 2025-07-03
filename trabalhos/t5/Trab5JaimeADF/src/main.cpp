a2aaff249b5359d4fd40a342a8760653fc387015
Jaime Daniel <jaimeadanielfilho@hotmail.com>
2025-07-03 13:23:32 -0300

    feat(trabalhos/t5): maximize material parameters of sun

--- trabalhos/t5/Trab5JaimeADF/src/main.cpp
+++ trabalhos/t5/Trab5JaimeADF/src/main.cpp
@@ -90,10 +90,10 @@ void InitializeModels()
     asteroid2dTexture->LoadFromBMP("Trab5JaimeADF/assets/models/asteroid_2d/asteroid_2d.bmp");
 
     auto sunMaterial = std::make_shared<Material>(
-        Vector4(1.0f, 0.8f, 0.3f, 1.0f),
-        Vector4(1.0f, 0.8f, 0.3f, 1.0f),
+        Vector4(1.0f, 1.0f, 1.0f, 1.0f),
+        Vector4(1.0f, 1.0f, 1.0f, 1.0f),
         Vector4(0.0f, 0.0f, 0.0f, 1.0f),
-        Vector4(1.0f, 0.8f, 0.3f, 1.0f),
+        Vector4(1.0f, 1.0f, 1.0f, 1.0f),
         0.0f
     );
 
